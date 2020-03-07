64216a0d33938e230cb0c4259f5b6052339bf9c3
louis baptiste <louisvis@ymail.com>
2020-03-07 05:30:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+function findMatching (drivers, string) {
+  const newDrivers = [];
+
+  for (const driver of drivers) {
+    if ( driver == string || driver.toLowerCase() == string.toLowerCase() ) {
+      newDrivers.push(driver);
+    }
+  }
+
+  return newDrivers;
+}
+
+
+function fuzzyMatch(drivers, string) {
+  const newDrivers = [];
+
+  for (const driver of drivers) {
+    if ( driver[0] == string[0]) {
+      newDrivers.push(driver);
+    }
+  }
+
+  return newDrivers;
+}
+
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
